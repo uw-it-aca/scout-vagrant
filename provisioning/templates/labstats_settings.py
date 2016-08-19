590c3086391bff9e1547c258104f1ae8adfbda53
Craig M. Stimmel <cstimmel@uw.edu>
2016-08-19 12:36:07 -0700

    Fix indentation on logging config.

--- provisioning/templates/labstats_settings.py
+++ provisioning/templates/labstats_settings.py
@@ -127,41 +127,41 @@ INSTALLED_APPS = (
 # See http://docs.djangoproject.com/en/dev/topics/logging for
 # more details on how to customize your logging configuration.
 LOGGING = {
-     'version': 1,
-     'disable_existing_loggers': False,
-     'filters': {
-     'require_debug_false': {
-     '()': 'django.utils.log.RequireDebugFalse'
-     },
- },
- 'handlers': {
-     'mail_admins': {
-     'level': 'ERROR',
-     'filters': ['require_debug_false'],
-     'class': 'django.utils.log.AdminEmailHandler'
-     },
-     'console': {
-     'level': 'WARNING',
-     'class': 'logging.StreamHandler',
-     },
-     'file': {
-     'level': 'DEBUG',
-     'class': 'logging.FileHandler',
-     'filename': '/home/vagrant/labstats_daemon.log',
-     }
- },
- 'loggers': {
-     'django.request': {
-     'handlers': ['mail_admins'],
-     'level': 'ERROR',
-     'propagate': True,
- },
- '': {
-     'handlers': ['console', 'file'],
-     'level': 'DEBUG',
-     'propagate': True,
- },
- }
+    'version': 1,
+    'disable_existing_loggers': False,
+    'filters': {
+    'require_debug_false': {
+            '()': 'django.utils.log.RequireDebugFalse'
+        },
+    },
+    'handlers': {
+        'mail_admins': {
+            'level': 'ERROR',
+            'filters': ['require_debug_false'],
+            'class': 'django.utils.log.AdminEmailHandler'
+        },
+        'console': {
+            'level': 'WARNING',
+            'class': 'logging.StreamHandler',
+        },
+        'file': {
+            'level': 'DEBUG',
+            'class': 'logging.FileHandler',
+            'filename': '/home/vagrant/labstats_daemon.log',
+        },
+    },
+    'loggers': {
+        'django.request': {
+            'handlers': ['mail_admins'],
+            'level': 'ERROR',
+            'propagate': True,
+        },
+        '': {
+            'handlers': ['console', 'file'],
+            'level': 'DEBUG',
+            'propagate': True,
+        },
+    }
 }
 
 # Make this unique, and don't share it with anybody.
